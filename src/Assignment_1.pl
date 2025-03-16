6ddf237de5b882923379d217660f2e90a5e4ba4e
Hana <hanaabdelbari882@gmail.com>
2025-03-17 01:35:08 +0200

    Making Task 2 about counting num of teams

--- src/Assignment_1.pl
+++ src/Assignment_1.pl
@@ -1,2 +1,8 @@
+:- consult('league_data.pl'). % Include the provided data file
+% Task 1
 players_in_team(Team, Player) :-
     player(Player, Team, _).
+% Task 2
+team_count_by_country(Country, Count) :-
+    findall(Team, team(Team, Country, _), Teams),
+    length(Teams, Count).
