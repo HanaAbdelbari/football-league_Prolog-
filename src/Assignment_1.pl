06d51244c74201631d6800526d36fbc37fdd3f12
kokom <ka.mahrous@gmail.com>
2025-03-19 01:18:59 +0200

    Task 1 and 2 commit

--- src/Assignment_1.pl
+++ src/Assignment_1.pl
@@ -1,8 +1,28 @@
 :- consult('league_data.pl'). % Include the provided data file
 % Task 1
-players_in_team(Team, Player) :-
-    player(Player, Team, _).
+players_in_team(Team, L):-
+    players_Acc(Team, L, []), !.
+
+players_Acc(Team, [Player|Rest], Acc):-
+    player(Player, Team, _),
+    \+ member(Player, Acc),
+    players_Acc(Team, Rest, [Player|Acc]).
+
+players_Acc(_, [], _).
+
+%-------------------------------------------------
 % Task 2
 team_count_by_country(Country, Count) :-
     findall(Team, team(Team, Country, _), Teams),
     length(Teams, Count).
+%-------------------------------------------------
+%Task 3
+%-------------------------------------------------
+
+
+
+
+
+
+
+
